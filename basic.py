de8d6bc0aaa609e96418242910e92d93506bf6c2
Raunit <raunitd.it.17@nsit.net.in>
2020-07-17 00:22:01 +0530

    Added position Class

--- basic.py
+++ basic.py
@@ -8,25 +8,40 @@ VALID_CHARS = '_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 # ERRORS
 ####################################
 class Error:
-    def __init__(self, error_name, details):
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
         self.error_name = error_name
         self.details = details
     
     def __repr__(self):
-        return f'{self.error_name}:{self.details}'
+        return f'{self.error_name}:{self.details}\nFile: {self.pos_start.file_name}, Line Number: {self.pos_start.line + 1}'
 
 class IllegalCharError(Error):
-    def __init__(self, details):
-        super().__init__('Illegal Character', details)
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Illegal Character', details)
 
 
 ####################################
 # POSITION
 ####################################
 class Position:
-    def __init__(self, line, column):
+    def __init__(self, index, line, column, file_name=None, file_text=None):
+        self.index = index
         self.line = line
         self.column = column
+        self.file_name = file_name
+        self.file_text = file_text
+    
+    def advance(self, current_char):
+        self.index += 1
+        self.column += 1
+        if current_char == '\n':
+            self.line += 1
+            self.column = 0
+        
+    def copy(self):
+        return Position(self.index, self.line, self.column, self.file_name, self.file_text)
 
     def __repr__(self):
         return f'{self.line}:{self.column}'
@@ -62,15 +77,16 @@ class Token:
 ####################################
 
 class Lexer:
-    def __init__(self, text):
+    def __init__(self, file_name, text):
+        self.file_name = file_name
         self.text = text
-        self.pos = -1
+        self.pos = Position(-1, 0, -1, self.file_name, self.text)
         self.current_char = None
         self.advance()
 
     def advance(self):
-        self.pos += 1
-        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.index] if self.pos.index < len(self.text) else None
     
     def make_number(self):
         period_count = 0
@@ -92,7 +108,7 @@ class Lexer:
     def make_tokens(self):
         tokens = []
         while self.current_char:
-            if self.current_char in [' ', '\t']:
+            if self.current_char in [' ', '\t', '\n']:
                 self.advance()
             elif self.current_char in token_types:
                 tokens.append(list(token_types[self.current_char].values())[0])
@@ -100,7 +116,10 @@ class Lexer:
             elif self.current_char in DIGITS:
                 tokens.append(self.make_number())
             else:
-                return [], IllegalCharError(f"'{self.current_char}' in line '{self.text}'")
+                pos_start = self.pos.copy()
+                char = self.current_char
+                self.advance()
+                return [], IllegalCharError(pos_start, self.pos, f"'{char}'")
         return tokens, None
 
 
@@ -109,7 +128,7 @@ class Lexer:
 # RUN
 #######################
 
-def run(text):
-    lexer = Lexer(text)
+def run(fn, text):
+    lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
     return tokens, error
--- shell.py
+++ shell.py
@@ -1,7 +1,7 @@
 import basic
 while True:
     text = input('basic > ')
-    result, error = basic.run(text)
+    result, error = basic.run('<stdin>', text)
     if error:
         print(error)
     else:
