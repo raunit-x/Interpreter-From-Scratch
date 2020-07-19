70ab73ea392f74efed5c62340b29161d026b027b
Raunit <raunitd.it.17@nsit.net.in>
2020-07-19 09:49:51 +0530

    updated the keywords

--- basic_interpreter/constants.py
+++ basic_interpreter/constants.py
@@ -2,4 +2,14 @@ import string
 DIGITS = '0123456789'
 LETTERS = string.ascii_letters
 LETTERS_DIGITS = LETTERS + DIGITS
-KEYWORDS = {'VAR', 'AND', 'OR', 'NOT'}
+
+KEYWORDS = {
+    'VAR', 
+    'AND',
+    'OR',
+    'NOT',
+    'IF',
+    'ELIF',
+    'ELSE',
+    'THEN'
+}
