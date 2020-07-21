23ae598cd84a87319c80ee212cab8717e412136a
Raunit <raunitd.it.17@nsit.net.in>
2020-07-22 00:20:16 +0530

    Added verbose error messages

--- basic_interpreter/errors.py
+++ basic_interpreter/errors.py
@@ -1,3 +1,5 @@
+from basic_interpreter.strings_with_arrows import string_with_arrows
+
 class Error:
     def __init__(self, pos_start, pos_end, error_name, details):
         self.pos_start = pos_start
@@ -5,8 +7,12 @@ class Error:
         self.error_name = error_name
         self.details = details
     
-    def __repr__(self):
-        return f'{self.error_name}:{self.details}\nFile: {self.pos_start.file_name}, Line: {self.pos_start.line + 1}'
+    def as_string(self):
+        result  = f'{self.error_name}: {self.details}\n'
+        result += f'File {self.pos_start.file_name}, line {self.pos_start.line + 1}'
+        result += '\n\n' + string_with_arrows(self.pos_start.file_text, self.pos_start, self.pos_end)
+        return result
+        
 
 class IllegalCharError(Error):
     def __init__(self, pos_start, pos_end, details):
@@ -20,9 +26,11 @@ class RunTimeError(Error):
     def __init__(self, pos_start, pos_end, details, context):
         super().__init__(pos_start, pos_end, 'Runtime Error', details)
         self.context = context
-    
-    def __repr__(self):
-        result = self.generate_traceback()
+
+    def as_string(self):
+        result  = self.generate_traceback()
+        result += f'{self.error_name}: {self.details}'
+        result += '\n\n' + string_with_arrows(self.pos_start.file_text, self.pos_start, self.pos_end)
         return result
     
     def generate_traceback(self):
--- shell.py
+++ shell.py
@@ -3,6 +3,6 @@ while True:
     text = input('basic > ')
     result, error = basic.run('<stdin>', text)
     if error:
-        print(error)
+        print(error.as_string())
     elif result:
         print(result)
